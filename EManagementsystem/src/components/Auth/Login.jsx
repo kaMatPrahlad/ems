021aac3367e07524ef5af6ca7da6c6b3a5eea1a1
KaMatprahlad <kamatprahlad43@gmail.com>
2025-05-08 19:58:53 +0530

    Login context

--- EManagementsystem/src/components/Auth/Login.jsx
+++ EManagementsystem/src/components/Auth/Login.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div className='flex items-center justify-center'>
-            <div className='border-2 border-red-600'>
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <div className='border-2 rounded-xl border-emerald-600 p-20'>
 
 
-                <form >
-                    <input type="email" placeholder='Enter your Email' />
-                    <input type="password" placeholder='Enter your password' />
-                    <p className='text-red-400'>hello</p>
+                <form className='flex flex-col items-center justify-center'>
+                    <input required className=' outline-none bg-transparent border-2 border-emerald-600 py-3 px-6 text-xl rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your Email' />
+                    <input required className=' outline-none bg-transparent mt-3 border-2 border-emerald-600 py-3 px-6 text-xl rounded-full placeholder:text-gray-400' type="password" placeholder='Enter your password' />
+                    <p className='text-red-400'></p>
+                    <button className='mt-7 text-white hover:bg-emerald-700 font-semibold outline-none border-none bg-emerald-600 py-3 px-6 text-xl rounded-full placeholder:text-white '>Login in</button>
                 </form>
             </div>
 
