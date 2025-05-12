4463c4de714ea31fe149e5949dd4bd20c0a62f39
KaMatprahlad <kamatprahlad43@gmail.com>
2025-05-12 20:34:36 +0530

    git commit

---
+++ EManagementsystem/src/components/other/TaskListNumbers.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TaskListNumbers = () => {
+    return (
+        <div className='flex mt-10 justify-between gap-5 w-screen '>
+            <div className='h-40 w-[45%] bg-red-400'>
+                <h2 className='text-2xl font-semibold'>0</h2>
+                <h3 className='text-xl font-medium'>New Task</h3>
+
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default TaskListNumbers
